8490c28a304f8c828e05f9f9628b336268848b39
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-17 21:13:58 +0700

    Write

--- Main.py
+++ Main.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import mysql.connector
+
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+st.write("Work")
