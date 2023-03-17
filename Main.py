2c7e3fba8ee23627f3c15bc0c9187580fd64c60c
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-17 21:35:51 +0700

    -

--- Main.py
+++ Main.py
@@ -4,11 +4,13 @@ import numpy as np
 
 import mysql.connector
 
-st.cache_resource
-def init_connection():
-    return mysql.connector.connect(st.secrets["mysql"])
 
-conn = init_connection()
-st.cache_data(ttl=600)
+st.write("DB username:",st.secrets["mysql"]["host"])
+# st.cache_resource
+# def init_connection():
+#     return mysql.connector.connect(st.secrets["mysql"])
 
-st.write("Work")
+# conn = init_connection()
+# st.cache_data(ttl=600)
+
+# st.write("Work")
