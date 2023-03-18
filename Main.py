bd5cd453d965a263b11eae2ce1d9198497515a2f
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-18 13:38:54 +0700

    -

--- Main.py
+++ Main.py
@@ -3,22 +3,17 @@ import mysql.connector
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
 
-conn = init_connection()
+
+mydb = mysql.connector.connect(
+        host=['mysql']['localhost'],
+        user=['mysql']['user'],
+        password=['mysql']['password'],
+        database= ['mysql']['database']
+    )
+
+mycursor = mydb.cursor()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
 
-rows = run_query("SELECT * from orang;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
