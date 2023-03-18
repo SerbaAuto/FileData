94cf3043888ba0f617ff2ba6874df129f53faede
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-18 14:40:27 +0700

    -

--- Main.py
+++ Main.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-@st.cache_data(ttl=600)
-
-def load_data(sheet_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
-#df = load_data(st.secrets["public_gsheets_url"])
-
 df = pd.read_csv(st.secrets['public_gsheet_csv'])
 
 # Print results.
