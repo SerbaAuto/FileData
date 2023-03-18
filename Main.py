70059ceacb536da78c1ba2a0d15e0a5bcb9f1a95
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-18 14:25:57 +0700

    -

--- Main.py
+++ Main.py
@@ -1,27 +1,14 @@
 import streamlit as st
-import mysql.connector
+import pandas as pd
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
+@st.cache_data(ttl=600)
 
-st.write(**st.secrets['mysql'])
-# mydb = mysql.connector.connect(
-#         host=st.secrets['mysql']['host'],
-#         user=st.secrets['mysql']['user'],
-#         password=st.secrets['mysql']['password'],
-#         database= st.secrets['mysql']['database'],
-#         port = st.secrets['mysql']['port']
-#     )
-st.write("Lewat")
-st.error("error")
-# mycursor = mydb.cursor()
-def init_connection():
-    mydb = mysql.connector.connect(**st.secrets["mysql"])
+def load_data(sheet_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
+df = load_data(st.secrets["public_gsheets_url"])
 
-init_connection()
-st.write("bisa")
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.id} has a :{row.name}:")
