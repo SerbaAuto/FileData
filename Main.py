8a67ef45ace5729f44ff90b5b939a7038bc06450
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-18 14:30:37 +0700

    -

--- Main.py
+++ Main.py
@@ -7,7 +7,9 @@ def load_data(sheet_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+#df = load_data(st.secrets["public_gsheets_url"])
+
+df = pd.read_csv(st.secrets['public_gsheet_csv'])
 
 # Print results.
 for row in df.itertuples():
